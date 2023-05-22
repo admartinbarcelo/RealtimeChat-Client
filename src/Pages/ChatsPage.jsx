17df24ba3da4acfa294640cbc398dbd2ebf539b0
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-22 18:51:10 +0200

    chat realtime

--- src/Pages/ChatsPage.jsx
+++ src/Pages/ChatsPage.jsx
@@ -2,12 +2,14 @@ import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatsPage = (props) => {
   return (
+    <div style={{ height: "100vh", width: "100vw" }}>
     <PrettyChatWindow
       projectId="c05c5b26-a8e9-43d1-bc5e-ce8872a8310d"
       username={props.user.username}
       secret={props.user.secret}
       style={{ height: "100vh" }}
     />
+    </div>
   );
 };
 
